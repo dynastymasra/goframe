aee13e6a321ac5930e53d7676c6bc294851a74b7
Dimas Ragil Triatmaja <dynastymasra@gmail.com>
2020-06-16 13:20:17 +0700

    Init const

---
+++ config/const.go
@@ -0,0 +1,25 @@
+package config
+
+const (
+	ServiceName = "Goframe"
+	Version     = "0.1.0"
+
+	// Service port address
+	envServerPort = "SERVER_PORT"
+	envGRPCPort   = "GRPC_PORT"
+
+	envServerKey = "SERVER_KEY"
+
+	// Logger config
+	envLoggerFormat = "LOGGER_FORMAT"
+	envLoggerLevel  = "LOGGER_LEVEL"
+
+	// Postgres config
+	envPostgresDBName      = "POSTGRES_DATABASE_NAME"
+	envPostgresAddress     = "POSTGRES_ADDRESS"
+	envPostgresUsername    = "POSTGRES_USERNAME"
+	envPostgresPassword    = "POSTGRES_PASSWORD"
+	envPostgresMaxIdleConn = "POSTGRES_MAX_IDLE_CONN"
+	envPostgresMaxOpenConn = "POSTGRES_MAX_OPEN_CONN"
+	envPostgresLogEnabled  = "POSTGRES_LOG_ENABLED"
+)
